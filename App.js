ca68b78edd70a6af7822f2a7540328753efa26c4
Gman-root <148723855+Gman-root@users.noreply.github.com>
2023-11-15 23:42:49 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,79 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+function FlashcardPage() {
+  const [flashcards, setFlashcards] = useState([]);
+  const [front, setFront] = useState('');
+  const [back, setBack] = useState('');
+  const [editingIndex, setEditingIndex] = useState(null);
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('http://localhost:5000/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
-      });
-      const data = await response.json();
-      setMessage(data.message);
-    } catch (error) {
-      console.error('Error:', error);
-      setMessage('Error connecting to the server');
-    }
+  const handleFrontChange = (event) => {
+    setFront(event.target.value);
+  };
+
+  const handleBackChange = (event) => {
+    setBack(event.target.value);
+  };
+
+  const handleCreateFlashcard = () => {
+    const newFlashcard = { front, back, flipped: false };
+    setFlashcards([...flashcards, newFlashcard]);
+    setFront('');
+    setBack('');
+  };
+
+  const handleEditFlashcard = (index) => {
+    setFront(flashcards[index].front);
+    setBack(flashcards[index].back);
+    setEditingIndex(index);
+  };
+
+  const handleUpdateFlashcard = () => {
+    const updatedFlashcards = [...flashcards];
+    updatedFlashcards[editingIndex] = { front, back, flipped: false };
+    setFlashcards(updatedFlashcards);
+    setFront('');
+    setBack('');
+    setEditingIndex(null);
+  };
+
+  const handleDeleteFlashcard = (index) => {
+    const updatedFlashcards = [...flashcards];
+    updatedFlashcards.splice(index, 1);
+    setFlashcards(updatedFlashcards);
+  };
+
+  const handleFlipCard = (index) => {
+    const updatedFlashcards = [...flashcards];
+    updatedFlashcards[index].flipped = !updatedFlashcards[index].flipped;
+    setFlashcards(updatedFlashcards);
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleLogin}>
-          <input 
-            type="text" 
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username" 
-          />
-          <input 
-            type="password" 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password" 
-          />
-          <button type="submit">Login</button>
-        </form>
-        {message && <p>{message}</p>}
-      </header>
+    <div>
+      <input value={front} onChange={handleFrontChange} placeholder="Front" />
+      <input value={back} onChange={handleBackChange} placeholder="Back" />
+      {editingIndex === null ? (
+        <button onClick={handleCreateFlashcard}>Create</button>
+      ) : (
+        <button onClick={handleUpdateFlashcard}>Update</button>
+      )}
+      <ul className="flashcard-list">
+        {flashcards.map((flashcard, index) => (
+          <li key={index} className={`flashcard ${flashcard.flipped ? 'flipped' : ''}`}>
+            <div className="flashcard-front" onClick={() => handleFlipCard(index)}>
+              {flashcard.flipped ? flashcard.back : flashcard.front}
+            </div>
+            <div className="flashcard-back" onClick={() => handleFlipCard(index)}>
+              {flashcard.flipped ? flashcard.front : flashcard.back}
+            </div>
+            <button onClick={() => handleEditFlashcard(index)}>Edit</button>
+            <button onClick={() => handleDeleteFlashcard(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default FlashcardPage;
